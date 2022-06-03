102a7210ceab8bc742fcfc4c93817112e94e1a2a
Mihail Dorozhko <dominick.alone@gmail.com>
2022-06-04 00:21:37 +0300

    feat: add solution cli/enj.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,11 @@
+import { env } from 'process';
+
 export const parseEnv = () => {
-    // Write your code here 
+  for (let value in env) {
+    if (value.includes("RSS_")) {
+      console.log(`${value}=${env[value]}`);
+    }
+  };
 };
+
+parseEnv();
