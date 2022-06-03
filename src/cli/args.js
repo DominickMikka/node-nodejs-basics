526fc49fb044fba3e308d96abaec27b7b328fc83
Mihail Dorozhko <dominick.alone@gmail.com>
2022-06-04 00:53:02 +0300

    feat: add solution cli/args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,16 @@
+import { argv } from 'process';
+
 export const parseArgs = () => {
-    // Write your code here 
+  let args = [];
+
+  for (let i = 0; i < argv.length; i++) {
+    if (argv[i].includes('--')) {
+      let str = `${argv[i].slice(2, argv[i].length)} is ${argv[i+1]}`;
+      args.push(str);
+    }
+  }
+  
+  console.log(args.join(', '));
 };
+
+parseArgs();
