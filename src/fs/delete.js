0dce0a62af84eb2faff238af48e41cf0d624758b
Mihail Dorozhko <dominick.alone@gmail.com>
2022-06-03 22:13:38 +0300

    feat: add solution fs/delete.js task

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,18 @@
+import { access, unlink } from 'fs/promises';
+import { fileURLToPath } from 'url';
+import { dirname, join} from 'path';
+
 export const remove = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const pathToFile = join(__dirname, 'files', 'fileToRemove.txt');
+
+  try {
+    await access(pathToFile);
+    await unlink(pathToFile);
+  } catch(err) {
+    throw new Error('FS operation failed');
+  }
 };
+
+remove();
