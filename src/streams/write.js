4ce9c947421b896c4670002ec611d03f2e7a0bc0
Mihail Dorozhko <dominick.alone@gmail.com>
2022-06-04 16:20:18 +0300

    feat: add solution streams/write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,19 @@
+import { open } from 'fs/promises';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
 export const write = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const pathToFile = join(__dirname, 'files', 'fileToWrite.txt');
+
+  const file = await open(pathToFile, 'w');
+  const stream = file.createWriteStream();
+  process.stdout.write('Please, write some text (if you want to exit use Ctrl+C):\n');
+
+  process.stdin.on('data', data => {
+    stream.write(data);
+  });
 };
+
+write();
