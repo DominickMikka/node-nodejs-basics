d91e733ea82c5a1b5385d3bfe3d5ec20a3733ee9
Mihail Dorozhko <dominick.alone@gmail.com>
2022-06-05 18:28:59 +0300

    feat: add solition streams/transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,15 @@
+import { Transform } from 'stream';
+
 export const transform = async () => {
-    // Write your code here 
+  const transformStream = new Transform({
+    transform(chunk, encoding, callback) {
+      this.push(chunk.toString().split('').reverse().join('') + '\n');
+      callback();
+    }
+  });
+
+  process.stdout.write('Please, write string (if you want to exit use Ctrl+C):\n');
+  process.stdin.pipe(transformStream).pipe(process.stdout);
 };
+
+transform();
